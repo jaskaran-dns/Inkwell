de3f230460e776e96133a59c282d730b4b620a4e
Jaskaran <105405723+jaskaran-dns@users.noreply.github.com>
2026-08-29 19:54:21 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
-import type { Database } from "@/types/database";
+import type { Database } from "@/database";
 
 /**
  * Server-side Supabase instance for use in Server Components, Route
